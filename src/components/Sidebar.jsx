6383752313dd065bd6dce4dd9048be26d2d5f853
GabrielGQA <gabrielgqa@gmail.com>
2023-04-27 17:32:26 -0300

    feat: finalize Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src='https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'/>
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Gabrielgqa.png" />
+               <Avatar src="https://github.com/Gabrielgqa.png" />
                 
                 <strong>Gabriel Almeida</strong>
                 <span>Web Developer</span>
