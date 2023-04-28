87cf91424b47f22859822dea4d0146e245e70f9a
GabrielGQA <gabrielgqa@gmail.com>
2023-04-28 16:07:25 -0300

    feat: form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
     function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -71,11 +78,13 @@ export function Post({ author, publishedAt, content }) {
                     name="comment" 
                     placeholder="Deixe um comentário"
                     value={newCommentText}
-                    onChange={handleNewCommentChange} 
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit" >Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer> 
             </form>
 
